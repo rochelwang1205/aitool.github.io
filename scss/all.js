6c502d0006aa2d554038a5c4a8434c6f14162009
rochelwang1205 <128132742+rochelwang1205@users.noreply.github.com>
2023-05-30 10:52:20 +0800

    Add files via upload
    LV.2.所有頁面RWD及動態效果

---
+++ scss/all.js
@@ -0,0 +1,30 @@
+$(function(){
+    $(".itm1").click(function(){
+        $(".panel1").slideToggle("slow");
+        $(".xs1").toggle();
+        $(".xs2").toggle();
+      });});
+$(function(){
+    $(".itm2").click(function(){
+        $(".panel2").slideToggle("slow");
+        $(".xs1").toggle();
+        $(".xs2").toggle();
+      });});
+$(function(){
+    $(".itm3").click(function(){
+        $(".panel3").slideToggle("slow");
+        $(".xs1").toggle();
+        $(".xs2").toggle();
+      });});
+$(function(){
+    $(".itm4").click(function(){
+        $(".panel4").slideToggle("slow");
+        $(".xs1").toggle();
+        $(".xs2").toggle();
+      });});
+$(function(){
+    $(".itm5").click(function(){
+        $(".panel5").slideToggle("slow");
+        $(".xs1").toggle();
+        $(".xs2").toggle();
+      });});
