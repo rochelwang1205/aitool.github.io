45e2dd6fb1158955683da07459f619f3b619edf6
rochelwang1205 <128132742+rochelwang1205@users.noreply.github.com>
2023-06-29 12:07:38 +0800

    Add files via upload
    adjust pricing page's header, footer, card part and  FAQ part by jQuery.

--- script/all.js
+++ script/all.js
@@ -28,23 +28,29 @@ const swiper = new Swiper('.swiper', {
 
 
 //panel slide
-$(document).ready(function() {
-  // 找到同層元素
-  $(".itms > .itm > h3").click(function(event) {
-    event.preventDefault();
-    var $panel = $(this).parent().find("p");
-    var $addIcon = $(this).find(".add-icon");
-    var $removeIcon = $(this).find(".remove-icon");
+// $(document).ready(function() {
+//   // 找到同層元素
+//   $(".itms > .itm > h3").click(function(event) {
+//     event.preventDefault();
+//     var $panel = $(this).parent().find("p");
+//     var $addIcon = $(this).find(".add-icon");
+//     var $removeIcon = $(this).find(".remove-icon");
 
-    $panel.slideToggle();
-    $addIcon.toggleClass("d-none");
-    $removeIcon.toggleClass("d-none");
+//     $panel.slideToggle();
+//     $addIcon.toggleClass("d-none");
+//     $removeIcon.toggleClass("add-icon");
 
-    // 收闔其他區塊
-    $(".itms > .itm > h3").not(this).parent().find("p").slideUp();
-    $(".itms > .itm > h3").not(this).find(".add-icon").removeClass("d-none");
-    $(".itms > .itm > h3").not(this).find(".remove-icon").addClass("d-none");
-  });
+//     // 收闔其他區塊
+//     $(".itms > .itm > h3").not(this).parent().find("p").slideUp();
+//     $(".itms > .itm > h3").not(this).find(".add-icon").removeClass("d-none");
+//     $(".itms > .itm > h3").not(this).find(".remove-icon").addClass("d-none");
+//   });
+// });
+$('.itm').click(function(e) {
+  $(this).toggleClass('active');
+  $(this).find('.add-icon').toggleClass('d-none');
+  $(this).find('.remove-icon').toggleClass('d-block');
+  $(this).find('.collapse-content p').toggleClass('show');
 });
 //back to top
 $(document).ready(function () {
